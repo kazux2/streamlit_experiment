75232f59b094a5361cc4e098d1fd3580827b144c
kazu <kazu@email>
2023-03-15 23:04:09 +0100

    bar chart + toggling button

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 # sample project https://docs.streamlit.io/library/get-started/create-an-app
 
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -30,8 +29,17 @@ if st.checkbox('Show raw data'):
 # create slider for specifying the number of data to show 
 day_to_filter = st.slider('day', 0, len(data.index), 50) 
 
-st.subheader('simple line chart')
+# create radio button
+graph_types = ['line_chart', 'bar_chart']
+selected_graph_type = st.radio('Graph type', graph_types)
+
+# prepare pandas data
 chart_data = data[[DATE_COLUMN, "building_household_emissions"]]
 chart_data = chart_data[chart_data.index<day_to_filter] # select rows by the comparison with the slider value 
-st.line_chart(chart_data, y="building_household_emissions")
 
+#toggle displayed graph based on selection
+if selected_graph_type == 'line_chart':
+    st.subheader('simple line chart')
+    st.line_chart(chart_data, y="building_household_emissions")
+else:
+    st.bar_chart(chart_data, x= "current_date", y="building_household_emissions")
